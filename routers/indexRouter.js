202b7ac2755aa400ea91c612ed4d08046e50b81e
Eduarda Guerra <eduardaguerra44@gmail.com>
2022-11-01 20:00:02 -0300

    Centralizando categorias, mudando padding perfil, arrumando router

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -7,11 +7,11 @@ const indexController = require('../controllers/indexController');
 const produtosController = require('../controllers/produtosController');
 
 // criando rotas - direcionando controller - indicando função
-router.get('/', indexController.showHome); 
+//router.get('/', indexController.showHome); 
 router.get('/cadastro', indexController.showCadastro);
 router.get('/login', indexController.showLogin);
 router.get('/finalizacao', indexController.showFinalizacao);
-router.get('/produtos', produtosController.listar);
+router.get('/', produtosController.listar);
 
 
 module.exports = router
